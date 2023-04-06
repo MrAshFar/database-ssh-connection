fb9b446e597e32efe1a4c32c6fee836f63d606f8
Ashkan Farmanbar <1992ashfar@gmail.com>
2023-04-06 15:20:33 +0430

    1st commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const { dbServer, forwardConfig, tunnelConfig } = require("./config.js");
+const mysql = require("mysql2");
+const { Client } = require("ssh2");
+
+if (process.env.NODE_ENV == "development") {
+  console.log("#------ dbServer -----------------------#");
+  console.log(`dbServer: ${JSON.stringify(dbServer)}`);
+  console.log("#------ forwardConfig ------------------#");
+  console.log(`forwardConfig: ${JSON.stringify(forwardConfig)}`);
+  console.log("#------ tunnelConfig -------------------#");
+  Object.keys(tunnelConfig).forEach((key) => console.log(`${key}:${tunnelConfig[key]}`));
+}
+// create an instance of SSH Client
+const sshClient = new Client();
+
+const SSHConnection = new Promise((resolve, reject) => {
+  sshClient
+    .on("ready", () => {
+      sshClient.forwardOut(forwardConfig.srcHost, forwardConfig.srcPort, forwardConfig.dstHost, forwardConfig.dstPort, (err, stream) => {
+        if (err) reject(err);
+
+        const updatedDbServer = {
+          ...dbServer,
+          stream,
+        };
+
+        const connection = mysql.createConnection(updatedDbServer);
+
+        connection.connect((error) => {
+          if (error) reject(error);
+          resolve(connection);
+        });
+      });
+    })
+    .connect(tunnelConfig);
+});
+
+SSHConnection.then(
+  (resolve) => {
+    console.log(`ssh resolved ${resolve}`);
+  },
+  (reject) => {
+    console.log(`ssh reject ${reject}`);
+  }
+);
---
+++ src/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports.dbServer = dbServer = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+module.exports.tunnelConfig = {
+  host: process.env.DB_SSH_HOST,
+  port: process.env.DB_SSH_HOST_PORT,
+  username: process.env.DB_SSH_USER,
+  privateKey: require("fs").readFileSync(process.env.PRIVATE_KEY_PATH),
+};
+
+module.exports.forwardConfig = {
+  srcHost: process.env.SRC_HOST, // any valid address
+  srcPort: process.env.SRC_PORT, // any valid port
+  dstHost: process.env.DB_HOST, // destination database
+  dstPort: process.env.DB_PORT, // destination port
+};
